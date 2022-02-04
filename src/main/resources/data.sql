58a136b43f48cf0ec483e66618b93e0c6f15b0bf
Lawani Oluwa <oluwa.lawani@umontreal.ca>
2022-02-04 09:38:01 -0500

    Update application properties and add sample of data in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into film(id, titre, description) values(1001, 'Spider-Man : No Way Home', 'Spider-Man: No Way Home is a phenomenal conclusion to the trilogy and Holland''s best outing as Spidey yet.');
+insert into acteur(id, nom, prenom) values(1112, 'Holland', 'Tom');
+insert into film_acteurs values(1001, 1112);
